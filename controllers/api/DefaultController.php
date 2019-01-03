b47788c2fb82ec79ed39dd5ceb454663979884a2
Ferhat CAKIR <ferhatcakir@example.com>
2019-01-03 22:45:53 +0300

    first commit

---
+++ controllers/api/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\Urun\controllers\api;
+
+
+/**
+ * Default controller for the `Urun` module
+ */
+class DefaultController extends \kouosl\base\controllers\api\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
